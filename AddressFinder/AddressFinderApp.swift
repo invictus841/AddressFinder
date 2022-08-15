2f1e28cbecb5ee4e8bdccc8441504650be7f9d0e
NT <usedforfunny@gmail.com>
2022-08-15 15:50:50 +0200

    Initial Commit

---
+++ AddressFinder/AddressFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AddressFinderApp.swift
+//  AddressFinder
+//
+//  Created by Alexandre Talatinian on 15/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct AddressFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
